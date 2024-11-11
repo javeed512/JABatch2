e4acb8d9c66e80186d5d7ad28c50456ca80eecf5
javeed512 <javeedmohammedjaveed@gmail.com>
2024-11-11 13:37:08 +0530

    SERVLETs Notes,Diagrams,Demos

---
+++ Servlets_JSP/webapp/src/main/java/com/hexaware/controller/EmployeeServlet.java
@@ -0,0 +1,47 @@
+package com.hexaware.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+@WebServlet("/EmployeeServlet")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public EmployeeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			PrintWriter   out =	response.getWriter();
+			
+							response.setContentType("text/html");
+			
+					out.print("Hello Friends!");
+					out.print("<h1>WELCOME TO SERVLET</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
