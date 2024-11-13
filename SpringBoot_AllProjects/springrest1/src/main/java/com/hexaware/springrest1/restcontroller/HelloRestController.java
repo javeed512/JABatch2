a330d7364d0dd55f89186901217fcb76d6be8cb9
javeed512 <javeedmohammedjaveed@gmail.com>
2024-11-13 13:15:06 +0530

    Spring rest api demos,notes,pdf,diagrams

---
+++ SpringBoot_AllProjects/springrest1/src/main/java/com/hexaware/springrest1/restcontroller/HelloRestController.java
@@ -0,0 +1,57 @@
+package com.hexaware.springrest1.restcontroller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController   // @Controller + @ResponseBody
+@RequestMapping("/api")
+public class HelloRestController {
+	
+	
+			@GetMapping("/hello")      // @RequestMapping(value="hello", method=Get)
+			public String    hello() {
+				
+				return "Hello Friends";
+				
+			}
+			
+			@GetMapping("/get")     
+			public String   get() {
+				
+				return "Get request executed";
+				
+			}
+			
+			
+			
+			@PostMapping("/add")
+			public  String    add() {
+				
+				return "Data Added using POST method";
+				
+				
+			}
+			
+			@PutMapping("/update")
+			public String  update() {
+				
+				
+				return "data updated using PUT method";
+				
+			}
+			
+			@DeleteMapping("/delete")
+			public String   remove() {
+				
+				return "data removed using DELETE method";
+				
+			}
+			
+			
+	
+
+}
